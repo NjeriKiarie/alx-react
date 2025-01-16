ef629fc5b469ebe82b0a8510a7f0bc5ad00adcf1
NjeriKiarie <kcarolnjeri@gmail.com>
2025-01-16 07:09:05 -0800

    CREATOR

--- 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
+++ 0x07-react_redux_action_creator_normalizr/task_2/dashboard/src/schema/notifications.js
@@ -1,7 +1,33 @@
-import notificationData from '../../../../notifications.json';
+import * as notificationItem from "../../notifications.json";
+import { normalize, schema } from 'normalizr';
 
-export default function getAllNotificationsByUser(userId) {
-	return notificationData
-		.filter((notification) => notification.author.id === userId)
-		.map((notification) => notification.context);
-}
+// Define a users schema
+const user = new schema.Entity('users');
+
+// Define a message schema
+const message = new schema.Entity('messages', {}, {
+  idAttribute: 'guid'
+});
+
+// Define a notification schema
+const notification = new schema.Entity('notifications', {
+  author: user,
+  context: message
+});
+
+export const normalizedData = normalize(notificationItem.default, [notification])
+
+export const getAllNotificationsByUser = (userId) => {
+
+  const entityNotification = normalizedData.entities.notifications;
+  const entityMessage = normalizedData.entities.messages;
+  const data = [];
+
+  for (let item in entityNotification) {
+    if (entityNotification[item].author === userId) {
+      const contextMessage = entityNotification[item].context;
+      data.push(entityMessage[contextMessage])
+    }
+  }
+  return data;
+};
